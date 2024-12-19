63626ff4f817b169a9ddf578d7851845ba68fb12
Berk Muzaffer Eldemir <beldemir@k1m09s08.42istanbul.com.tr>
2024-12-19 08:28:33 +0300

    initial commit.

---
+++ push_swap.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beldemir <beldemir@42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 07:51:09 by beldemir          #+#    #+#             */
+/*   Updated: 2024/12/19 08:28:13 by beldemir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	isarg_int(char *arg)
+{
+	int	number;
+
+	while ()
+}
+
+int main(int ac, char *av)
+{
+	int *a;
+	int *b;
+	int i;
+	int	c;
+
+	if (ac < 2)
+		return (0);
+	i = 0;
+	while (av[++i])
+		if (isarg_int(av[i]) == -1)
+			return (write(1, "Error\n", 6), -1);
+	a = (int *)malloc(sizeof(int) * i);
+	i = 0;
+	while (av[++i])
+		a[i - 1] = ft_atoi(av[i]);
+	
+	free(a);
+}
