11c331a3673c1e9f61315535a141ee8c7bbb6f2c
Berk Muzaffer Eldemir <beldemir@k1m36s02.42istanbul.com.tr>
2025-03-01 12:55:46 +0300

    controls are done. (probably)

--- utils_2.c
+++ utils_2.c
@@ -6,7 +6,7 @@
 /*   By: beldemir <beldemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:56:42 by beldemir          #+#    #+#             */
-/*   Updated: 2025/02/28 16:10:48 by beldemir         ###   ########.fr       */
+/*   Updated: 2025/03/01 12:53:13 by beldemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,23 @@ int	new_atoi(const char *str, int *tab_num)
 {
 	int		i;
 	long	num;
-	int		isneg;
+	int		sign;
 
 	i = 0;
 	num = 0;
-	isneg = 1;
+	sign = 1;
 	while (str[i] != '\0' && (str[i] == 32 || (str[i] <= 13 && str[i] >= 9)))
 		i++;
-	if (str[i] != '\0' && str[i++] == '-')
-		isneg *= -1;
-	else if (str[i] == '+')
-		i++;
+	if (str[i] != '\0' && (str[i] == '-' || str[i] == '+'))
+		if (str[i++] == '-')
+			sign *= -1;
 	while (str[i] != '\0' && ft_isdigit(str[i]) == 1)
 	{
-		num = (num * 10) + (str[i] - 48);
-		i++;
-        if ((long)(num * (long)isneg) > 2147483647 || \
-        (long)(num * (long)isneg) < -2147483648)
+		num = (num * 10) + (str[i++] - 48);
+		if ((num * (long)sign) > 2147483647 || \
+        (num * (long)sign) < -2147483648)
             return (-1);
 	}
-    *tab_num = num;
+    *tab_num = num * sign;
 	return (1);
 }
